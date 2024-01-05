414861e7451f6cb6517a139b044acba8f759c207
Roger <roger.levy@gmail.com>
2024-01-04 21:31:54 -0500

    wip
    wip
    
    wip
    
    cleanup
    
    fixed point
    
    
    misc, toggle
    
    
    delta, safety net
    
    bigger, consolas
    
    early shed "hello world"
    
    improve repl toggling
    
    add ability to move game window within the display
    
    reorganize, fix word not found bug, add decadv23 submod
    
    log
    
    create shed, add darkblue starfield
    
    starfield; arbitrary object stacks; dedicated REPL object stack
    
    fix corrupt first input
    
    dragging
    
    hovering, fixy
    
    halt stage in ui
    
    big wip (includes fixing transforms and other things)
    
    info
    
    .
    
    serialize
    
    nested mouse interaction
    
    deserialize
    
    .
    
    custom serializtaion
    
    .
    
    drag entire staage
    
    save stage position
    
    bg
    
    gradient
    
    delete, fix reckon
    
    fix del garbage, tile snapping
    
    uiext
    
    add mersenne twister from zeptoforth
    
    snow
    
    using a vertex list
    
    .
    
    xmas
    
    megaphone, speedup
    
    event more speedup
    
    using advice from the manual for using the tokenizer when you have "Return Stack Modifiers"
    
    add foggy scene background
    
    fix interpreted *actor, fix ui you
    
    .
    
    .
    
    wip
    
    done / not done
    
    fix timer / general improvements
    
    Move main loop into shed.vfx
    
    don't auto build anymore
    
    load snow.vfx from main.vfx
    
    move dll's to their own folder, fixy
    
    animation system wip
    
    engine improvements
    
    instantiation / serialization overhaul
    
    named objects
    
    move main loop to core.vfx
    
    fix modifiers sticking when switch out
    
    bigger repl
    
    picking overhaul
    
    absolute transforms
    speedup
    
    timing overhaul
    
    debug fixy
    
    animation system test
    
    make objects quit on error
    
    hovering: fix size check bug

---
+++ assets/dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="dungeon" tilewidth="16" tileheight="16" tilecount="171" columns="19">
+ <image source="../assets/dungeon.png" width="304" height="144"/>
+</tileset>
